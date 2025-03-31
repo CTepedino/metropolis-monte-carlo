55fa8c2d8e44e88ec04b6d8352dacfac9f1909af
CTepedino <ctepedino@itba.edu.ar>
2025-03-31 12:58:17 -0300

    Save animation as file

--- src/main/python/gridVisualizer.py
+++ src/main/python/gridVisualizer.py
@@ -1,18 +1,21 @@
+import os
 import sys
 
 import numpy as np
 
 import matplotlib
 
+
 from readFile import readFile, startIterator, nextGrid, closeIterator
 
-matplotlib.use("TkAgg")
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 
-
 if __name__ == "__main__":
+    os.makedirs('animations', exist_ok=True)
+
     global ani
     if len(sys.argv) > 1:
         matrix_file_path = sys.argv[1]
@@ -20,23 +23,21 @@ if __name__ == "__main__":
         matrix_file_path = "output.txt"
 
     N, p, steps, grid = startIterator(matrix_file_path)
-    step = 0
 
     fig, ax = plt.subplots()
     cax = ax.imshow(grid, cmap="gray", vmin=-1, vmax=1)
-    title = ax.set_title("monte carlo step 0")
     ax.set_xticks([])
     ax.set_yticks([])
 
     def update(frame):
-        global step
         global grid
-        step += 1
         grid = nextGrid(grid)
         cax.set_array(grid)
-        title.set_text(f"monte carlo step {frame}")
-        return cax
+        return [cax]
 
-    ani = animation.FuncAnimation(fig, update, frames=steps, interval=1, blit=False)
+    ani = animation.FuncAnimation(fig, update, frames=5000, interval=1, blit=True)
 
-    plt.show()
+    output_path = os.path.join('animations', f'animation_n_{N}_p_{p}.mp4')
+    writer = animation.FFMpegWriter(fps=60, bitrate=1800)
+
+    ani.save(output_path, writer=writer)
