506d79ba4c7199617f788e5cc116edc1dd0394e7
CTepedino <ctepedino@itba.edu.ar>
2025-03-31 11:49:05 -0300

    Improved visualizer loading time

--- src/main/python/gridVisualizer.py
+++ src/main/python/gridVisualizer.py
@@ -4,7 +4,7 @@ import numpy as np
 
 import matplotlib
 
-from readFile import readFile
+from readFile import readFile, startIterator, nextGrid, closeIterator
 
 matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
@@ -19,19 +19,24 @@ if __name__ == "__main__":
     else:
         matrix_file_path = "output.txt"
 
-    N, p, grids = readFile(matrix_file_path)
+    N, p, steps, grid = startIterator(matrix_file_path)
+    step = 0
 
     fig, ax = plt.subplots()
-    cax = ax.imshow(grids[0], cmap="gray", vmin=-1, vmax=1)
+    cax = ax.imshow(grid, cmap="gray", vmin=-1, vmax=1)
     title = ax.set_title("monte carlo step 0")
     ax.set_xticks([])
     ax.set_yticks([])
 
     def update(frame):
-        cax.set_array(grids[frame])
+        global step
+        global grid
+        step += 1
+        grid = nextGrid(grid)
+        cax.set_array(grid)
         title.set_text(f"monte carlo step {frame}")
-        return cax, title
+        return cax
 
-    ani = animation.FuncAnimation(fig, update, frames=len(grids), interval=1, blit=False)
+    ani = animation.FuncAnimation(fig, update, frames=steps, interval=1, blit=False)
 
     plt.show()
